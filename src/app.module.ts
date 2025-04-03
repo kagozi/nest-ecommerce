aaf1e785c2e094b46759f4a7eeb135a3be327ee4
ALEX KAGOZI <alexkagozi@96-2-234-62-static.midco.net>
2025-04-03 16:53:08 -0500

    ++ Products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     UsersModule,
-    AuthModule],
+    AuthModule,
+    ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
