bbc432922de5bc20acd11b01c78dcc7366b124ef
ALEX KAGOZI <alexkagozi@96-2-234-62-static.midco.net>
2025-04-03 16:51:42 -0500

    ++ JWT auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.db',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
