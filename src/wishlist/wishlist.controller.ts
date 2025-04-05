67980bfbd72c5cf7264ee431592965e901a3af36
ALEX KAGOZI <alexkagozi@96-2-234-62-static.midco.net>
2025-04-04 23:50:34 -0500

    ++ Wishlist module

--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Delete, Get, Param, Req } from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import { Request } from 'express';
 
 @Controller('wishlist')
-export class WishlistController {}
+export class WishlistController {
+  constructor(private readonly wishlistService: WishlistService) {}
+
+  @Post(':productId')
+  addToWishlist(@Req() req: Request, @Param('productId') productId: number) {
+    // @ts-ignore
+    const userId = req.user.id;
+    return this.wishlistService.addToWishlist(userId, productId);
+  }
+
+  @Delete(':productId')
+  removeFromWishlist(@Req() req: Request, @Param('productId') productId: number) {
+    // @ts-ignore
+    const userId = req.user.id;
+    return this.wishlistService.removeFromWishlist(userId, productId);
+  }
+
+  @Get()
+  viewWishlist(@Req() req: Request) {
+    // @ts-ignore
+    const userId = req.user.id;
+    return this.wishlistService.viewWishlist(userId);
+  }
+}
--- src/wishlist/wishlist.module.ts
+++ src/wishlist/wishlist.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { WishlistController } from './wishlist.controller';
+import { Wishlist } from './wishlist.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
+   imports: [
+        TypeOrmModule.forFeature([Wishlist]), forwardRef(() => UsersModule), forwardRef(() => ProductsModule), // Adjust the import path as necessary
+      ],
   providers: [WishlistService],
   controllers: [WishlistController]
 })
